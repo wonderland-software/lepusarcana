2d85dba13e5d3219ac0be1f4515947863aebe7ed
Tag Locklar <tlocklar3@gmail.com>
2025-08-31 13:44:08 -0500

    Yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import lepusarcanaImage from "./assets/lepusarcana.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-content">
+        <img src={lepusarcanaImage} className="App-image" alt="Lepus Arcana" />
+      </div>
     </div>
   );
 }
